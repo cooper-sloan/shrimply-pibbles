8269d94be057833f970c5ed4044164733a05fbad
dotunadegbite <dotunadegbite23@gmail.com>
2016-09-17 17:48:18 -0400

    Adding arrays of instruments

---
+++ instrumentArrays.js
@@ -0,0 +1,60 @@
+var bass = [ "acoustic_bass",
+  "electric_bass_finger",
+  "electric_bass_pick",
+  "fretless_bass",
+  "lead_8_bass__lead",
+  "slap_bass_1",
+  "slap_bass_2",
+  "synth_bass_1",
+  "synth_bass_2"];
+
+
+var drums =[
+  "synth_drum"];
+
+var piano = ["acoustic_grand_piano",
+  "bright_acoustic_piano",
+  "electric_grand_piano",
+  "electric_piano_1",
+  "electric_piano_2"];
+
+var guitar = [ "acoustic_guitar_nylon",
+  "acoustic_guitar_steel",
+  "distortion_guitar",
+  "electric_guitar_clean",
+  "electric_guitar_jazz",
+  "electric_guitar_muted",
+  "guitar_fret_noise",
+  "guitar_harmonics",
+  "overdriven_guitar"];
+
+var percussion = ["marimba", "glockenspiel","tinkle_bell",
+  "tubular_bells", "steel drums"];
+
+var voice = ["lead_6_voice",
+  "voice_oohs", "choir_aahs",
+  "pad_4_choir",
+  "synth_choir"];
+
+var fx = ["fx_1_rain",
+  "fx_2_soundtrack",
+  "fx_3_crystal",
+  "fx_4_atmosphere",
+  "fx_5_brightness",
+  "fx_6_goblins",
+  "fx_7_echoes",
+  "fx_8_scifi"];
+var brass = ["tuba", "muted_trumpet",
+  "trumpet", "trombone",  "brass_section",
+  "synth_brass_1",
+  "synth_brass_2"];
+
+var orchestra = ["violin", "cello", "string_ensemble_1",
+  "string_ensemble_2",
+  "synth_strings_1",
+  "synth_strings_2"];
+
+var woodwinds = ["flute", "alto_sax",
+  "baritone_sax",
+  "soprano_sax",
+  "tenor_sax"];
